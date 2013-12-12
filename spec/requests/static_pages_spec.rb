5b27656cd261158d154784ca12845ef4c4e544d8
Daniel Kreuter <daniel.kreuter@1und1.de>
2013-12-12 13:43:52 +0100

    Refactored redundant title part into a variable Added contact page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+
 describe "Static Pages" do
+  let (:title) {
+    $title = "Ruby on Rails Tutorial Sample App | "
+  }
   describe "Home pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +13,7 @@ describe "Static Pages" do
 
     it "should have the title Home" do
       visit "/static_pages/home"
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{title} Home")
     end
 
   end
@@ -22,7 +26,7 @@ describe "Static Pages" do
 
     it "should have the title Help" do
       visit "/static_pages/help"
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{title} Help")
     end
   end
 
@@ -34,9 +38,16 @@ describe "Static Pages" do
 
     it "should have the title About Us" do
       visit "/static_pages/about"
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      expect(page).to have_title("#{title} About Us")
     end
 
+  end
+
+  describe "Contact page" do
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+    end
   end
 
 end
